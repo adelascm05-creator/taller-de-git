febb4893882e3670cbad585617ee8c3fe8360805
Adela <adelascm05@gmail.com>
2025-10-15 13:01:13 +0200

    Programa interactivo

--- lib/hola.php
+++ lib/hola.php
@@ -1,5 +1,5 @@
 <?php
 // Autor: Adela Sánchez-Crespo Moñino <adelascm05@gmail.com>
-// El nombre por defecto es Mundo
-$nombre = isset($argv[1]) ? $argv[1] : "Mundo";
+print "Introduce tu nombre:";
+$nombre = trim(fgets(STDIN));
 @print "Hola, {$nombre}\n";

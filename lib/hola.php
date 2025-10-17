40175c69f25b2af03dccf6d82491093c2fd46d75
Adela <adelascm05@gmail.com>
2025-10-17 09:24:28 +0200

    Programa interactivo

--- lib/hola.php
+++ lib/hola.php
@@ -1,5 +1,5 @@
 <?php
-// Autor: Adela Sánchez-Crespo Moñino <adelascm05@gmail.com>
+// Autor: Adela <adelascm05@gmail.com>
 print "Introduce tu nombre:";
 $nombre = trim(fgets(STDIN));
 @print "Hola, {$nombre}\n";
